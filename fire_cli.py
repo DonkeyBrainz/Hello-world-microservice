953e13595ffbfde8c644d872aa00187cfa641d07
mowgli <46568362+mowgl-i@users.noreply.github.com>
2022-04-20 01:02:18 +0000

    new function, test and cli function

--- fire_cli.py
+++ fire_cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import fire # pylint: disable=import-error
-from lib.function_one import wiki
+import fire  # pylint: disable=import-error
+from lib.function_one import wiki, search_wiki  # pylint: disable=unused-import
 
 if __name__ == "__main__":
-    fire.Fire(wiki)
+    fire.Fire()
