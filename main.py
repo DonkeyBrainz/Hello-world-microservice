e5049d9587862a94f19d070255e1f58cf6228f75
mowgli <46568362+mowgl-i@users.noreply.github.com>
2022-04-20 02:07:30 +0000

    added docker and build docker

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI # pylint: disable=import-error
+import uvicorn # pylint: disable=import-error
+from lib import function_one
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Fast and Simple Wiki API. Call /search or /wiki in url"}
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Scrape wiki for provided value"""
+
+    results = function_one.search_wiki({value})
+    return {"results": results}
+
+@app.get("/wiki/{value}")
+async def wiki(value: str):
+    """Get wiki page for provided value"""
+
+    results = function_one.wiki({value})
+    return {"results": results}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
